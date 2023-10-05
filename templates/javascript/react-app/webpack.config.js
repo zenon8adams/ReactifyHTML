4f186a4948fd7ffe04d24631cd97b728ec9670c5
Meekness Adesina <zenon8adams@gmail.com>
2023-10-05 09:54:04 +0100

    Source/Template/Fix: css-loader should not resolve absolute urls
    Signed-off-by: Meekness Adesina <zenon8adams@gmail.com>

--- templates/javascript/react-app/webpack.config.js
+++ templates/javascript/react-app/webpack.config.js
@@ -42,7 +42,17 @@ module.exports = () => {
                 },
                 {
                     test: /\.css$/i,
-                    use: ['style-loader', 'css-loader', 'postcss-loader'],
+                    use: [
+                        'style-loader', {
+                            loader: 'css-loader',
+                            options: {
+                            url: {
+                                    filter: url => url[0] !== '/'
+                                }
+                            }
+                        },
+                        'postcss-loader'
+                    ],
                     sideEffects: true,
                 },
                 {
